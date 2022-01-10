b0bc2f44103a332ef68b4b4113d55f285a0c6795
suedaislak <suedakinci@gmail.com>
2022-01-10 14:44:12 -0500

    work 3 is done

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,5 +2,7 @@ public class HelloWord {
 
     //Doing some work here(you can type anything you want)
 //doing awesome work 2!
-//work3.
+//doing awesome work 3!
+    //awesome work 3 is done and ready to be committed
+    //look at the history
 }
